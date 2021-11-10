c01d8b0f7eba6c3f5a1c0b0b9cc766274b5785df
Jacky <dangkky@gmail.com>
2021-11-10 21:36:27 +0800

    1110 P656 马上结束第二阶段

---
+++ src/com/jacky/tankgame5/Bomb.java
@@ -0,0 +1,24 @@
+package com.jacky.tankgame5;
+
+/**
+ * 2021/11/4
+ */
+public class Bomb {
+    int x, y;
+    int life = 9;  //炸弹的生命周期
+    boolean isLive = true;
+
+    public Bomb(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void lifeDown() {
+        if (life > 0) {
+            life--;
+        } else {
+            isLive = false;
+        }
+    }
+
+}
---
+++ src/com/jacky/tankgame5/Shot.java
@@ -0,0 +1,53 @@
+package com.jacky.tankgame5;
+
+/**
+ * 2021/11/4
+ */
+public class Shot implements Runnable {
+    int x;
+    int y;
+    int direct = 0;
+    int speed = 2;
+    boolean isLive = true;//判断子弹是否还存在
+
+    public Shot(int x, int y, int direct) {
+        this.x = x;
+        this.y = y;
+        this.direct = direct;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //根据方向改变x y坐标
+            switch (direct) {
+                case 0:
+                    y -= speed;
+                    break;
+                case 1:
+                    x += speed;
+                    break;
+                case 2:
+                    y += speed;
+                    break;
+                case 3:
+                    x -= speed;
+                    break;
+            }
+
+//            System.out.println("子弹x= " + x + " y= " + y);
+            //当子弹碰到敌人时，也应该结束线程
+            if (!(x >= 0 && x <= 1000 && y >= 0 && y <= 750 && isLive)) {
+                isLive = false;
+                break;
+            }
+        }
+    }
+}
